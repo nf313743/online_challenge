63b9bd1b68f7b02514c253c1318d420c5c569507
nathan313743 <nathandevelopmain@gmail.com>
2017-09-03 17:26:07 +0100

    array rotation more pythonic

--- hackerrank/arrays_left_rotation/arrays_left_rotation.py
+++ hackerrank/arrays_left_rotation/arrays_left_rotation.py
@@ -2,8 +2,7 @@ class ArrayLeftRotation:
 
     # Longer but easier on the memory
     def array_left_rotation1(self, a, arr_length, shifts):
-        shifts_done = 0
-        while shifts_done < shifts:
+        for _ in range(shifts):
             index = arr_length - 1
             next_val = a[index]
             while index >= 0:
@@ -12,15 +11,11 @@ class ArrayLeftRotation:
                 a[index_to_move_to] = next_val
                 next_val = temp
                 index -= 1
-            shifts_done += 1
         return a
 
     def array_left_rotation2(self, a, arr_length, shifts):
         arr = [None] * arr_length
-        index = 0
-
-        while index < arr_length:
+        for index in range(arr_length):
             index_to_move_to = (index - shifts + arr_length) % arr_length
             arr[index_to_move_to] = a[index]
-            index += 1
         return arr
